3266d0fc02ebbc81dbedee3130d370f7707b6dc1
Benjaming Bingham <ben@benbingham.ca>
2024-08-12 09:39:31 -0600

    Initial commit

---
+++ install.sh
@@ -0,0 +1,3 @@
+install okularGenerator_poppler.so /usr/lib64/qt5/plugins/okular/generators
+install fragment.spv /usr/lib64/qt5/plugins/okular/generators
+install vertex.spv /usr/lib64/qt5/plugins/okular/generators
