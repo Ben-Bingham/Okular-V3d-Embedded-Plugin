d7887153e58258c332e33efae86e2ef508ff199e
Benjamin Bingham <ben@benbingham.ca>
2024-08-14 14:21:32 -0600

    Updated install file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-install okularGenerator_poppler.so /usr/lib64/qt5/plugins/okular/generators
-install fragment.spv /usr/lib64/qt5/plugins/okular/generators
-install vertex.spv /usr/lib64/qt5/plugins/okular/generators
+install okularGenerator_poppler.so /usr/lib64/qt6/plugins/okular/generators
+install fragment.spv /usr/lib64/qt6/plugins/okular/generators
+install vertex.spv /usr/lib64/qt6/plugins/okular/generators
