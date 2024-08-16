a0e58d8bd706e64a4799bca423b811bb92995046
Benjamin Bingham <ben@benbingham.ca>
2024-08-16 11:59:08 -0600

    Added another path where shaders are installed

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 install okularGenerator_poppler.so /usr/lib64/qt6/plugins/okular_generators
+
 install fragment.spv /usr/lib64/qt6/plugins/okular_generators
 install vertex.spv /usr/lib64/qt6/plugins/okular_generators
+
+install fragment.spv /usr/bin/
+install vertex.spv /usr/bin/
